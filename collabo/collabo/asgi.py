855abbd5404451f73832693be714dd5539c0758a
romeoscript <romeobourne211@gmail.com>
2025-09-05 20:01:40 +0100

    added information html

--- collabo/collabo/asgi.py
+++ collabo/collabo/asgi.py
@@ -14,3 +14,16 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collabo.settings')
 
 application = get_asgi_application()
+
+
+we should try to make a change to the code
+
+this is a new day , lets have fun
+this is a fixed information
+this is a new day , lets have fun
+
+this is a new day , lets have fun
+
+this is a new day , lets have fun
+
+this is a new change
